3613d2341f0b4c5d89598010515a0a0d7a9dfd54
rakshitha436 <rakshitha2514@gmail.com>
2026-03-26 09:48:57 +0530

    Update qa_pipeline.py

--- qa_pipeline.py
+++ qa_pipeline.py
@@ -23,7 +23,7 @@ User Question: {question}
 Answer:""")
 
     llm = ChatGoogleGenerativeAI(
-        model="gemini-2.0-flash",
+        model="gemini-2.5-flash",
         google_api_key=os.getenv("GOOGLE_API_KEY"),
         temperature=0
     )
